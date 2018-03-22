6d9d2477ae7161298723840bb72a9751bc2d739c
Jude <julie_wang1@brown.edu>
2018-03-22 03:18:37 -0400

    sandbox under construction + fonts displaying now

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import combos from "./data/fontCombos.json"
+import fillers from "./data/fillers.json"
 
 class Home extends Component {
   render() {
@@ -15,13 +16,25 @@ class Home extends Component {
   }
 }
 
+function fontLabel(combo) {
+  var str = "";
+  str += combo.h1 + "/";
+  str += combo.h2["font-family"] + "/";
+  str += combo.text;
+  return <p class="label">{str}</p>;
+}
+
 function hierarchy(props) {
+  const rand = Math.floor(Math.random() * fillers.length);
+  const filler = fillers[rand];
+
   return (
     <div class="masonrywrapper">
       <div class="masonryitem">
-        <h3>{props.h1}</h3>
-        <h4>{props.h2}</h4>
-        <p>{props.text}</p>
+        <h3 style={{"font-family": props.h1}}>{filler["Title"]}</h3>
+        <p class="subtitle" style={props.h2}>{filler["Subtitle"]}</p>
+        <p style={{"font-family": props.text}}>{filler["Dummy"]}</p>
+        <p>{fontLabel(props)}</p>
       </div>
     </div>
   );
